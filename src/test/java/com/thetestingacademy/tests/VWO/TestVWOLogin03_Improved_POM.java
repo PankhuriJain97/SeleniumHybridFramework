05ee1fbf2c04a325739129dcf9409153277cf763
PankhuriJain97 <pankhurijain97@gmail.com>
2026-06-07 15:10:41 +0530

    new

---
+++ src/test/java/com/thetestingacademy/tests/VWO/TestVWOLogin03_Improved_POM.java
@@ -0,0 +1,28 @@
+package com.thetestingacademy.tests.VWO;
+
+import com.thetestingacademy.pages.POM.VWO.LoginPage;
+import com.thetestingacademy.utils.PropertiesReader;
+import io.qameta.allure.Description;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class TestVWOLogin03_Improved_POM {
+
+    @Test
+    @Description("Invalid Login")
+    public void invalid_login() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://app.vwo.com");
+
+        LoginPage loginPage = new LoginPage(driver);
+        String error_msg = loginPage.Login_invalid(PropertiesReader.readKey("invalid_username"), PropertiesReader.readKey("invalid_password"));
+
+        assertThat(error_msg).isNotNull().isNotEmpty().isNotBlank();
+        Assert.assertEquals(error_msg,PropertiesReader.readKey("error_message"));
+
+    }
+}
