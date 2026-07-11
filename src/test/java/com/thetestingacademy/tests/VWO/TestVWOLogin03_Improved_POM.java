69a0347f6424ca00ad85d5548457e2f68dfd0d8e
PankhuriJain97 <pankhurijain97@gmail.com>
2026-07-11 23:45:05 +0530

    new

--- src/test/java/com/thetestingacademy/tests/VWO/TestVWOLogin03_Improved_POM.java
+++ src/test/java/com/thetestingacademy/tests/VWO/TestVWOLogin03_Improved_POM.java
@@ -1,21 +1,21 @@
 package com.thetestingacademy.tests.VWO;
 
-import com.thetestingacademy.base.CommonToAll;
 import com.thetestingacademy.baseTest.CommonToAllTest;
-import com.thetestingacademy.driver.DriverManager;
 import com.thetestingacademy.pages.POM.VWO.LoginPage;
 import com.thetestingacademy.utils.PropertiesReader;
 import io.qameta.allure.Description;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import static com.thetestingacademy.driver.DriverManager.driver;
 import static com.thetestingacademy.driver.DriverManager.getDriver;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
 public class TestVWOLogin03_Improved_POM extends CommonToAllTest {
+
+    private static final Logger logger = LogManager.getLogger(TestVWOLogin03_Improved_POM.class);
 
     @Test
     @Description("Invalid Login")
@@ -24,9 +24,15 @@ public class TestVWOLogin03_Improved_POM extends CommonToAllTest {
 
         //driver.get(PropertiesReader.readKey("url"));
 
+        logger.info("Browser is started...");
+
         LoginPage loginPage = new LoginPage(getDriver());
+
+        logger.info("verifying invalid credentials ....");
         String error_msg = loginPage.Login_invalid(PropertiesReader.readKey("invalid_username"), PropertiesReader.readKey("invalid_password"));
 
+        logger.info("Assertions...");
+        logger.info(error_msg);
         assertThat(error_msg).isNotNull().isNotEmpty().isNotBlank();
         Assert.assertEquals(error_msg,PropertiesReader.readKey("error_message"));
 
